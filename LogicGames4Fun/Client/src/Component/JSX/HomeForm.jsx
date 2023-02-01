ad13d29bc4125cfb3420b747e18f5e1b12aec8d9
Jakub Kliś <jakubklis2201@gmail.com>
2023-02-01 09:18:11 +0100

    Added empty line

--- LogicGames4Fun/Client/src/Component/JSX/HomeForm.jsx
+++ LogicGames4Fun/Client/src/Component/JSX/HomeForm.jsx
@@ -37,6 +37,7 @@ export const HomeForm = () => {
             will be adapted to people of all ages, through different levels of
             difficulty.
           </p1>
+          <br></br>
           <a href="/Login">
             <button id="getStarted-button">Get started</button>
           </a>
