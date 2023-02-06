c3806f850edda6745c17f00fc77fe48684dd6e52
Jakub Kliś <jakubklis2201@gmail.com>
2023-02-06 22:40:44 +0100

    Added modal forms to EditProfile

---
+++ LogicGames4Fun/Client/src/Component/EditData/EditProfileForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export const EditProfileForm = (props) => {
+  return (
+    <>
+      <div>
+        <Dialog open={props.open} onClose={props.handleClose}>
+          <DialogTitle>Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Edit profile
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={props.handleClose}>Cancel</Button>
+            <Button onClick={props.handleClose}>Subscribe</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    </>
+  );
+};
--- LogicGames4Fun/Client/src/Component/JSX/ProfileForm.jsx
+++ LogicGames4Fun/Client/src/Component/JSX/ProfileForm.jsx
@@ -1,13 +1,52 @@
 import React from "react";
 import "../CSS/ProfileForm.css";
+import { EditProfileForm } from "../EditData/EditProfileForm";
+import { EditPhotoForm } from "../EditData/EditPhotoForm";
+import { EditPasswordForm } from "../EditData/EditPasswordForm";
 
 export const ProfileForm = () => {
+  // Edit profile
+  const [openEditProfile, setOpenEditProfile] = React.useState(false);
+
+  const handleClickOpenEditProfile = () => {
+    setOpenEditProfile(true);
+  };
+
+  const handleCloseEditProfile = () => {
+    setOpenEditProfile(false);
+  };
+
+  // Edit password
+  const [openEditPassword, setOpenEditPassword] = React.useState(false);
+
+  const handleClickOpenEditPassword = () => {
+    setOpenEditPassword(true);
+  };
+
+  const handleCloseEditPassword = () => {
+    setOpenEditPassword(false);
+  };
+
+  // Edit photo
+  const [openEditPhoto, setOpenEditPhoto] = React.useState(false);
+
+  const handleClickOpenEditPhoto = () => {
+    setOpenEditPhoto(true);
+  };
+
+  const handleCloseEditPhoto = () => {
+    setOpenEditPhoto(false);
+  };
+
   return (
     <>
+      <EditProfileForm open={openEditProfile} setOpen={setOpenEditProfile} handleClickOpen={handleClickOpenEditProfile} handleClose={handleCloseEditProfile}/>
+      <EditPasswordForm open={openEditPassword} setOpen={setOpenEditPassword} handleClickOpen={handleClickOpenEditPassword} handleClose={handleCloseEditPassword}/>
+      <EditPhotoForm open={openEditPhoto} setOpen={setOpenEditPhoto} handleClickOpen={handleClickOpenEditPhoto} handleClose={handleCloseEditPhoto}/>
       <div className="myProfile-page-content">
         <div id="myProfile-info">
           <div className="user-avatar">
-            <img
+            <img onClick={handleClickOpenEditPhoto}
               src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar1.png`}
               alt="User avatar"
             ></img>
@@ -30,8 +69,8 @@ export const ProfileForm = () => {
           </div>
           <div className="myProfile-buttons">
             <div className="change-data-buttons">
-              <button id="button">Edit profile</button>
-              <button id="button">Edit password</button>
+              <button onClick={handleClickOpenEditProfile} id="button">Edit profile</button>
+              <button onClick={handleClickOpenEditPassword} id="button">Edit password</button>
             </div>
             <div className="back-to-dashboard-button">
               <a href="/Dashboard">
@@ -42,7 +81,7 @@ export const ProfileForm = () => {
         </div>
         <div id="myProfile-achievements">
           <div className="achievements-title">
-            <h1>Achievements</h1>
+            <h1>My achievements</h1>
           </div>
         </div>
       </div>
