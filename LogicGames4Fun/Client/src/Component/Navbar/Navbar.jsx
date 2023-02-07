045469b84cc9b57a6bbc945c463738456f01250e
Jakub Kliś <jakubklis2201@gmail.com>
2023-02-08 00:10:57 +0100

    Added SearchBox to Navbar

--- LogicGames4Fun/Client/src/Component/Navbar/Navbar.jsx
+++ LogicGames4Fun/Client/src/Component/Navbar/Navbar.jsx
@@ -13,10 +13,18 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
 export const Navbar = () => {
   const pages = ["Find game"];
-  const settings = ["🪪 Profile", "❤️ Favourite games", "🏆 Achievements", "🐾 Logout"];
+  const settings = [
+    "🪪 Profile",
+    "❤️ Favourite games",
+    "🏆 Achievements",
+    "🐾 Logout",
+  ];
 
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
@@ -35,6 +43,47 @@ export const Navbar = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: "20ch",
+      },
+    },
+  }));
+
   return (
     <>
       <div id="dashboard-page-content">
@@ -47,7 +96,7 @@ export const Navbar = () => {
               <img
                 src={`${process.env.PUBLIC_URL}/Images/NavbarLogo.png`}
                 alt="Logic Games 4Fun Logo"
-                style={{ width:"64px", height:"64px"}}
+                style={{ width: "64px", height: "64px" }}
               ></img>
               <Typography
                 variant="h6"
@@ -94,14 +143,9 @@ export const Navbar = () => {
                   sx={{
                     display: { xs: "block", md: "none" },
                   }}
-                >
-                  {pages.map((page) => (
-                    <MenuItem key={page} onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">{page}</Typography>
-                    </MenuItem>
-                  ))}
-                </Menu>
+                ></Menu>
               </Box>
+
               <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
               <Typography
                 variant="h5"
@@ -122,15 +166,14 @@ export const Navbar = () => {
                 LOGO
               </Typography>
               <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                {pages.map((page) => (
-                  <Button
-                    key={page}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: "white", display: "block" }}
-                  >
-                    {page}
-                  </Button>
-                ))}
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    placeholder="Search…"
+                  />
+                </Search>
               </Box>
 
               <Box sx={{ flexGrow: 0 }}>
@@ -161,7 +204,10 @@ export const Navbar = () => {
                   {settings.map((setting) => (
                     <MenuItem key={setting} onClick={handleCloseUserMenu}>
                       {/* <Typography textAlign="center">{setting}</Typography> */}
-                      <a href = {`/${setting.slice(2).replaceAll(' ', '')}`}> {setting}</a>
+                      <a href={`/${setting.slice(2).replaceAll(" ", "")}`}>
+                        {" "}
+                        {setting}
+                      </a>
                     </MenuItem>
                   ))}
                 </Menu>
