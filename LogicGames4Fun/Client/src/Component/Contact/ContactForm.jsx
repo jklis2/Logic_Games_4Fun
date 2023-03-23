03072713c6f17dde6b69616e9915f58a38769063
Matd127 <79983536+Matd127@users.noreply.github.com>
2023-03-23 14:54:32 +0100

    Contact page responsiveness

--- LogicGames4Fun/Client/src/Component/Contact/ContactForm.jsx
+++ LogicGames4Fun/Client/src/Component/Contact/ContactForm.jsx
@@ -1,47 +1,50 @@
 import React from "react";
-import styles from "./ContactForm.module.css";
+import styles from "./ContactForm.module.scss";
+import "../UI/Button/Button.scss";
 
 export const ContactForm = () => {
   return (
     <>
-      <div className={styles["contact-page-content"]}>
-        <div id={styles["contact-logo-text"]}>
-          <div id={styles["conttac-page-logo"]}>
-            <img
-              src={`${process.env.PUBLIC_URL}/Images/LogoLogicGames4Fun_smaller.png`}
-              alt="Logic Games 4Fun Logo"
-            ></img>
+      <div className={styles['contact-form-container']}>
+        <div className={`${styles["contact-page-content"]} bg-light w-75`}>
+          <div className={styles["contact-logo-text"]}>
+            <div className="d-flex flex-center justify-content-center">
+              <img
+                src={`${process.env.PUBLIC_URL}/Images/LogoLogicGames4Fun_smaller.png`}
+                alt="Logic Games 4Fun Logo"
+              ></img>
+            </div>
+            <div>
+              <h2>Contact Us</h2>
+            </div>
           </div>
-          <div id={styles["contact-page-text"]}>
-            <h2>Contact Us</h2>
+          <div id={styles["contact-form"]}>
+            <div id={styles["contact-send-message-form"]} action="">
+              <input
+                className={styles["contact-input"]}
+                type="text"
+                placeholder="Your Name"
+              ></input>
+              <input
+                className={styles["contact-input"]}
+                type="email"
+                placeholder="Your Email"
+              ></input>
+              <textarea
+                className={styles["contact-input"]}
+                rows="3"
+                placeholder="Your Message"
+              ></textarea>
+            </div>
           </div>
-        </div>
-        <div id={styles["contact-form"]}>
-          <div id={styles["contact-send-message-form"]} action="">
-            <input
-              className={styles["contact-input"]}
-              type="text"
-              placeholder="Your Name"
-            ></input>
-            <input
-              className={styles["contact-input"]}
-              type="email"
-              placeholder="Your Email"
-            ></input>
-            <textarea
-              className={styles["contact-input"]}
-              rows="3"
-              placeholder="Your Message"
-            ></textarea>
+          <div className={styles["contact-buttons"]}>
+            <a href="/Home">
+              <button className="button-light">Back to home</button>
+            </a>
+            <span>
+              <button className="button-light">Send message</button>
+            </span>
           </div>
-        </div>
-        <div className={styles["contact-buttons"]}>
-          <a href="/Home">
-            <button id={styles["backToHomeButton"]}>Back to home</button>
-          </a>
-          <span>
-            <button id={styles["sendMessageButton"]}>Send message</button>
-          </span>
         </div>
       </div>
     </>
