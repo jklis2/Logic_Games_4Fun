c17cc07bc2162b0f771f7fb706dc3e2458c33928
Matd127 <79983536+Matd127@users.noreply.github.com>
2023-10-12 14:05:41 +0200

    Update achievements styles

--- LogicGames4Fun/Client/src/Component/Achievements/AchievementsForm.jsx
+++ LogicGames4Fun/Client/src/Component/Achievements/AchievementsForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import styles from "./AchievementsForm.module.scss";
 import { Navbar } from "../Navbar/Navbar";
 import DashboardFooter from "../Dashboard/DashboardFooter";
 import ProgressBarValue from "./ProgressBarValue";
+import { achievementList } from "./achievementList";
 
 export const AchievementsForm = () => {
   return (
@@ -10,212 +10,24 @@ export const AchievementsForm = () => {
       <Navbar />
       <h1 className="h1 text-center my-5">Achievements</h1>
       <div className="container">
-        <div className="row justify-content-center">
-          {/* Sudoku achievements */}
-          <div className="col-md-4 achievement-card bg-light p-3 row">
-            <div className="col-4 d-flex align-items-center">
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/SudokuImages/SudokuNoviceAchievement.png`}
-                alt="Sudoku Navoice"
-                className="w-100"
-              ></img>
+        <div className="row">
+          {achievementList.map((achievement) => (
+            <div key={achievement.id} className="col-md-3 p-5">
+              <div className="bg-light achievement-card row">
+                <div className="col-4 p-4 d-flex align-items-center">
+                  <img
+                    src={achievement.img}
+                    alt={achievement.alt}
+                    className="w-100"
+                  ></img>
+                </div>
+                <div className="col-8 d-flex justify-content-center flex-column">
+                  <p className="fs-3">{achievement.name}</p>
+                  <ProgressBarValue />
+                </div>
+              </div>
             </div>
-            <div className="col-8 d-flex justify-content-center flex-column">
-              <p className="fs-3">Finish 25 easy levels of Sudoku</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          {/* end */}
-
-          <div className="achievement-card bg-light p-3 col-md-4 row">
-            <div className="col-4 d-flex align-items-center">
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/SudokuImages/SudokuProficientAchievement.png`}
-                alt="Sudoku Proficient"
-                className="w-100"
-              ></img>
-            </div>
-            <div className="col-8 d-flex justify-content-center flex-column">
-              <p className="fs-3">Finish 25 medium levels of Sudoku</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-
-          <div className="achievement-card mx-1 bg-light p-3 col-md-4 row">
-            <div className="col-4 d-flex align-items-center">
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/SudokuImages/SudokuMasterAchievement.png`}
-                alt="Sudoku Master"
-                className="w-100"
-              ></img>
-            </div>
-            <div className="col-8 d-flex justify-content-center flex-column">
-              <p className="fs-3">Finish 25 hard levels of Sudoku</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/SudokuImages/SudokuKingAchievement.png`}
-                alt="Sudoku King"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish all levels of Sudoku</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-
-          {/* Crossword achievements */}
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/CrosswordImages/CrosswordNoviceAchievement.png`}
-                alt="Crossword Navoice"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 easy levels of Crossword</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/CrosswordImages/CrosswordProficientAchievement.png`}
-                alt="Crossword Proficient"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 medium levels of Crossword</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/CrosswordImages/CrosswordMasterAchievement.png`}
-                alt="Crossword Master"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 hard levels of Crossword</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/CrosswordImages/CrosswordKingAchievement.png`}
-                alt="Crossword King"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish all levels of Crossword</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-
-          {/* Puzzle achievements */}
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/PuzzleImages/PuzzleNoviceAchievement.png`}
-                alt="Puzzle Navoice"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 easy levels of Puzzle</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/PuzzleImages/PuzzleProficientAchievement.png`}
-                alt="Puzzle Proficient"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 medium levels of Puzzle</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/PuzzleImages/PuzzleMasterAchievement.png`}
-                alt="Puzzle Master"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 hard levels of Puzzle</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/PuzzleImages/PuzzleKingAchievement.png`}
-                alt="Puzzle King"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish all levels of Puzzle</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-
-          {/* Quiz achievements */}
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/QuizImages/QuizNoviceAchievement.png`}
-                alt="Quiz Navoice"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 easy levels of Quiz</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/QuizImages/QuizProficientAchievement.png`}
-                alt="Quiz Proficient"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 medium levels of Quiz</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/QuizImages/QuizMasterAchievement.png`}
-                alt="Quiz Master"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish 25 hard levels of Quiz</p>
-              <ProgressBarValue />
-            </div>
-          </div>
-          <div className={styles.achievement}>
-            <div className={styles["achievements-image"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/AchievementImages/QuizImages/QuizKingAchievement.png`}
-                alt="Quiz King"
-              ></img>
-            </div>
-            <div className={styles["achievements-description"]}>
-              <p>Finish all levels of Quiz</p>
-              <ProgressBarValue />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <DashboardFooter />
---
+++ LogicGames4Fun/Client/src/Component/Achievements/achievementList.js
@@ -0,0 +1,98 @@
+export const achievementList = [
+  {
+    id: 1,
+    name: "Finish 25 easy levels of Sudoku",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/SudokuImages/SudokuNoviceAchievement.png`,
+    alt: "Sudoku Navoice",
+  },
+  {
+    id: 2,
+    name: "Finish 25 medium levels of Sudoku",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/SudokuImages/SudokuProficientAchievement.png`,
+    alt: "Sudoku Proficient",
+  },
+  {
+    id: 3,
+    name: "Finish 25 hard levels of Sudoku",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/SudokuImages/SudokuMasterAchievement.png`,
+    alt: "Sudoku Master",
+  },
+  {
+    id: 4,
+    name: "Finish all levels of Sudoku",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/SudokuImages/SudokuKingAchievement.png`,
+    alt: "Sudoku King",
+  },
+  {
+    id: 5,
+    name: "Finish 25 easy levels of Crossword",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/CrosswordImages/CrosswordNoviceAchievement.png`,
+    alt: "Crossword Navoice",
+  },
+  {
+    id: 6,
+    name: "Finish 25 medium levels of Crossword",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/CrosswordImages/CrosswordProficientAchievement.png`,
+    alt: "Crossword Proficient",
+  },
+  {
+    id: 7,
+    name: "Finish 25 hard levels of Crossword",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/CrosswordImages/CrosswordMasterAchievement.png`,
+    alt: "Crossword Master",
+  },
+  {
+    id: 8,
+    name: "Finish all levels of Crossword",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/CrosswordImages/CrosswordKingAchievement.png`,
+    alt: "Crossword King",
+  },
+  {
+    id: 9,
+    name: "Finish 25 easy levels of Puzzle",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/PuzzleImages/PuzzleNoviceAchievement.png`,
+    alt: "Puzzle Navoice",
+  },
+  {
+    id: 10,
+    name: "Finish 25 medium levels of Puzzle",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/PuzzleImages/PuzzleProficientAchievement.png`,
+    alt: "Puzzle Proficient",
+  },
+  {
+    id: 11,
+    name: "Finish 25 hard levels of Puzzle",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/PuzzleImages/PuzzleMasterAchievement.png`,
+    alt: "Puzzle Master",
+  },
+  {
+    id: 12,
+    name: "Finish all levels of Puzzle",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/PuzzleImages/PuzzleKingAchievement.png`,
+    alt: "Puzzle King",
+  },
+  {
+    id: 13,
+    name: "Finish 25 easy levels of Quiz",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/QuizImages/QuizNoviceAchievement.png`,
+    alt: "Quiz Navoice",
+  },
+  {
+    id: 14,
+    name: "Finish 25 medium levels of Quiz",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/QuizImages/QuizProficientAchievement.png`,
+    alt: "Quiz Proficient",
+  },
+  {
+    id: 15,
+    name: "Finish 25 hard levels of Quiz",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/QuizImages/QuizMasterAchievement.png`,
+    alt: "Quiz Master"
+  },
+  {
+    id: 16,
+    name: "Finish all levels of Quiz",
+    img: `${process.env.PUBLIC_URL}/AchievementImages/QuizImages/QuizKingAchievement.png`,
+    alt: "Quiz King"
+  }
+];
