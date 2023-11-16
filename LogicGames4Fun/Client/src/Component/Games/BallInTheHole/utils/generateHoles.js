8773560d16b0febbd9d3d27ac9f6759155a3e52c
Jakub Kliś <jakubklis2201@gmail.com>
2023-11-16 14:07:52 +0100

    Fixed repeatable bugs with holes

--- LogicGames4Fun/Client/src/Component/Games/BallInTheHole/utils/generateHoles.js
+++ LogicGames4Fun/Client/src/Component/Games/BallInTheHole/utils/generateHoles.js
@@ -1,62 +1,51 @@
 export function generateHoles(
-  count,
-  maxX,
-  maxY,
-  padding = 10,
-  holeSize = 40,
-  mathResult,
-  difficulty
+    count,
+    maxX,
+    maxY,
+    padding = 10,
+    holeSize = 40,
+    mathResult
 ) {
-  const holes = [];
-  const variance = 0.30;
-  const maxAttempts = 100;
+    const holes = [];
+    const usedNumbers = new Set();
+    const increment = 8; 
+    const variance = 8; 
 
-  const generateVarianceNumber = (baseNumber, isGreater) => {
-      return isGreater ? baseNumber * (1 + variance) : baseNumber * (1 - variance);
-  };
+    usedNumbers.add(mathResult); 
 
-  const generateUniqueVarianceNumber = (baseNumber, usedNumbers) => {
-      let attempts = 0;
-      let uniqueNumber;
-      do {
-          uniqueNumber = Math.round(generateVarianceNumber(baseNumber, Math.random() < 0.5));
-          if (attempts > maxAttempts) {
-              uniqueNumber = Math.round(baseNumber * (2 + Math.random()));
-              break;
-          }
-          attempts++;
-      } while (usedNumbers.has(uniqueNumber));
-      usedNumbers.add(uniqueNumber);
-      return uniqueNumber;
-  };
+    for (let i = 0; i < count; i++) {
+        let newHole;
+        let isOverlapping;
+        do {
+            isOverlapping = false;
+            let result;
+            if (i === 0) {
+                result = mathResult; 
+            } else {
+                do {
+                    const direction = Math.random() < 0.5 ? -1 : 1;
+                    result = mathResult + direction * (Math.floor(Math.random() * increment) + Math.floor(Math.random() * variance) - variance / 2);
+                } while (usedNumbers.has(result)); 
+                usedNumbers.add(result); 
+            }
 
-  const usedNumbers = new Set();
-  if (mathResult !== null) {
-      usedNumbers.add(mathResult);
-  }
+            newHole = {
+                left: Math.random() * (maxX - holeSize),
+                top: Math.random() * (maxY - holeSize),
+                result: result,
+            };
 
-  for (let i = 0; i < count; i++) {
-      let newHole;
-      let isOverlapping;
-      do {
-          isOverlapping = false;
-          newHole = {
-              left: Math.random() * (maxX - holeSize),
-              top: Math.random() * (maxY - holeSize),
-              result: i === 0 && mathResult !== null ? mathResult : generateUniqueVarianceNumber(mathResult, usedNumbers),
-          };
-
-          for (const hole of holes) {
-              const dx = newHole.left - hole.left;
-              const dy = newHole.top - hole.top;
-              const distance = Math.sqrt(dx * dx + dy * dy);
-              if (distance < holeSize + padding) {
-                  isOverlapping = true;
-                  break;
-              }
-          }
-      } while (isOverlapping);
-      holes.push(newHole);
-  }
-  return holes;
+            for (const hole of holes) {
+                const dx = newHole.left - hole.left;
+                const dy = newHole.top - hole.top;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                if (distance < holeSize + padding) {
+                    isOverlapping = true;
+                    break;
+                }
+            }
+        } while (isOverlapping);
+        holes.push(newHole);
+    }
+    return holes;
 }
