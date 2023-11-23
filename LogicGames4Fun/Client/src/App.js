df221043bae06f3f42913499be51cc9bde739523
Jakub Kliś <jakubklis2201@gmail.com>
2023-11-23 14:26:11 +0100

    Fixed icons in navbar

--- LogicGames4Fun/Client/src/App.js
+++ LogicGames4Fun/Client/src/App.js
@@ -20,7 +20,6 @@ import { Maze } from "./Routers/Maze";
 import { BallInheHole } from "./Routers/BallInTheHole";
 import { Quiz } from "./Routers/Quiz";
 import { TicTacToe } from "./Routers/TicTacToe";
-import { Test } from "./Routers/Test";
 
 function App() {
   return (
@@ -46,7 +45,6 @@ function App() {
         <Route path="/games/ballInTheHole" element={<BallInheHole />} />
         <Route path="/games/Quiz" element={<Quiz />} />
         <Route path="/games/ticTacToe" element={<TicTacToe />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
