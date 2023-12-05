ae38329e90720fe48ae136541a7f4ce93bb0ada6
Jakub Kliś <jakubklis2201@gmail.com>
2023-12-05 10:23:13 +0100

    Added new profile images

--- LogicGames4Fun/Client/src/Component/EditData/EditPhotoForm.jsx
+++ LogicGames4Fun/Client/src/Component/EditData/EditPhotoForm.jsx
@@ -52,6 +52,70 @@ export const EditPhotoForm = (props) => {
               src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar10.png`}
               alt="User avatar 10"
             ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar11.png`}
+              alt="User avatar 11"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar12.png`}
+              alt="User avatar 12"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar13.png`}
+              alt="User avatar 13"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar14.png`}
+              alt="User avatar 14"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar15.png`}
+              alt="User avatar 15"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar16.png`}
+              alt="User avatar 16"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar17.png`}
+              alt="User avatar 17"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar18.png`}
+              alt="User avatar 18"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar19.png`}
+              alt="User avatar 19"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar20.png`}
+              alt="User avatar 20"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar21.png`}
+              alt="User avatar 21"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar22.png`}
+              alt="User avatar 22"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar23.png`}
+              alt="User avatar 23"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar24.png`}
+              alt="User avatar 24"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar25.png`}
+              alt="User avatar 25"
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/UserAvatars/UserAvatar26.png`}
+              alt="User avatar 26"
+            ></img>
           </DialogContent>
           <DialogActions sx={{display: "flex", justifyContent: "center"}}>
             <Button onClick={props.handleClose}>Cancel</Button>
